05839b42fa073f2468ed1f6a0d6a76e427fd576f
LBeckX <lb@1601.com>
2017-06-12 13:21:05 +0200

    Fixing command line interface call without path. Check the defalult from config

--- RP2-Framework/class/api/module/bbRpc_setUrl.php
+++ RP2-Framework/class/api/module/bbRpc_setUrl.php
@@ -19,8 +19,10 @@ class bbRpc_setUrl extends apiModule
             return true;
         }
         else {
+
             // Get the correct path on http AND ssh/bash
             $path = isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : $_SERVER['PWD'];
+
             preg_match("/\\/.*\\/(\\d*)_\\d*\\/.*/", $path, $matches);
 
             if (isset($matches[1]) && intval($matches[1]) > 1)
@@ -28,7 +30,14 @@ class bbRpc_setUrl extends apiModule
                 $this->setUrl(sprintf(RPF_API_MODULE_BBRPC_SETURL_PATTERN, $matches[1]));
                 log::debug("Fetching rpcUrl from filesystem: {$this->rpcUrl}", __METHOD__);
                 return true;
-            } else {
+            }
+            elseif (RPF_API_MODULE_BBRPC_AUTH_DF_ORDER !== false){
+                $dfOrder = explode("_",RPF_API_MODULE_BBRPC_AUTH_DF_ORDER);
+                $this->setUrl(sprintf(RPF_API_MODULE_BBRPC_SETURL_PATTERN, $dfOrder[0]));
+                log::debug("Fetching rpcUrl from config.php individual df order: {$this->rpcUrl}", __METHOD__);
+                return true;
+            }
+            else {
                 throw new exception("You're running a this script not at your df-server OR you have a realy old server-setup. Please set your RP2-Auftragsnummer in config. See: https://github.com/ADoebeling/RP2-Framework/issues/15");
             }
         }
