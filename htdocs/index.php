a7efeeb0637c86094acd7451c4027248a6c76838
LBeckX <lb@1601.com>
2017-06-12 10:57:42 +0200

    Fixing von Bugs, Beispiel-Script erstellt, Test von allen Moeglichkeiten des Ausfuehrens, Fixing von Fehler im RPF-Log

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,19 @@
 <?php
+header("Content-Type: text/html; charset=utf-8");
 
 require_once __DIR__ . '/../3rd-party/RP2-Framework/bootstrap.php';
 
 $mySqlBackup = new \rpf\extension\module\mySqlBackup();
 
+$mySqlBackup->setBackupFolderPath(__DIR__."/backups_tmp");
+
 $databases = $mySqlBackup->getDatabaseInformations();
+
+$mySqlBackup->createFullBackup($databases);
+
+foreach ($databases as $database){
+    $mySqlBackup->createSingleBackup($database['hostip'],$database['pk'],$database['password'],$database['name']);
+}
+
+$mySqlBackup->sendProtocol("lb@1601.com");
+
